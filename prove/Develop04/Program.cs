c10e346e999e38bd3fa3aa9013f81b26174f6575
jacksynappiah <jerryjackson45545@yahoo.com>
2024-02-11 02:35:13 +0000

    W04_Prove Project done

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+       int breatheCount = 0;
+        int reflectCount = 0;
+        int listCount = 0;
+
+        // Console.WriteLine("Hello Develop04 World!");
+        Console.WriteLine("Welcome to the Mindfulness Program!");
+        Console.WriteLine();
+        bool running = true;
+        while (running)
+        {
+            Console.WriteLine("Menu Options: ");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflecting Activity");
+            Console.WriteLine("3. Listing Activity");
+            Console.WriteLine("4. Quit");
+            Console.WriteLine();
+            Console.Write("Select a choice from the menu: ");
+            int choice = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            switch (choice)
+            {   
+                //Breathing
+                case 1:
+                    Console.Clear();
+                    BreathingActivity breathing = new BreathingActivity();
+                    breathing.RunBreathingActivity();
+                    breatheCount++;
+                    break;
+                //Reflecting
+                case 2:
+                    Console.Clear();
+                    ReflectingActivity reflection = new ReflectingActivity();
+                    reflection.RunReflectingActivity();
+                    reflectCount++;
+                    break;
+                //Listing
+                case 3:
+                    Console.Clear();
+                    ListingActivity listing = new ListingActivity();
+                    listing.RunListingActivity();
+                    listCount++;
+                    break;
+                //quit
+                case 4:
+                    running = false;
+                    Console.WriteLine("Great job! You completed the following activities: ");
+                    Console.WriteLine($"Breathing Activity: {breatheCount} times");
+                    Console.WriteLine($"Reflecting Activity: {reflectCount} times");
+                    Console.WriteLine($"Listing Activity: {listCount} times");
+                    Console.WriteLine();
+                    Console.WriteLine("Thank you. Have a nice day!");
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Invalid input. Please choose one of the following activities.");
+                    break;
+            }
+     
+        }
     }
 }
